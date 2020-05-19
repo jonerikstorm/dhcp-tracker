68652e14bd524518ebdf185cb528f52b59ac28f8
Jon-Erik G. Storm <52981332+jonerikstorm@users.noreply.github.com>
2020-05-18 18:50:39 -0700

    Create package.sh

---
+++ package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cp dhcp-update /opkg/data/etc/dhcp-update
+pushd opkg/control/
+tar --numeric-owner --group=0 --owner=0 -czf ../control.tar.gz ./*
+popd
+
+pushd opkg/data/
+tar --numeric-owner --group=0 --owner=0 -czf ../data.tar.gz ./*
+popd
+
+pushd opkg
+tar --numeric-owner --group=0 --owner=0 -cf ../dhcp-update-1.0.0.ipk ./debian-binary ./data.tar.gz ./control.tar.gz 
+popd
